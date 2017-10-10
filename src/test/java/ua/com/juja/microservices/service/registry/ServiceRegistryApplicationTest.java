c6c944e754dfe9f7e7f2f0f22e1f58dd3fecf6b4
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-10-10 13:51:30 +0300

    #3: Add task for build docker images into build.gradle and first test for testing loading spring context

---
+++ src/test/java/ua/com/juja/microservices/service/registry/ServiceRegistryApplicationTest.java
@@ -0,0 +1,19 @@
+package ua.com.juja.microservices.service.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Danil Kuznetsov (kuznetsov.danil.v@gmail.com)
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceRegistryApplicationTest {
+    @Test
+    public void contextLoads() {
+
+    }
+}
